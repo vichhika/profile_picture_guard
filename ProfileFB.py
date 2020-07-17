c0e45261524aa36da12495bf4877d754d2b3bcd2
Vichhika <51940586+vichhika@users.noreply.github.com>
2020-07-17 17:55:10 +0700

    Update ProfileFB.py

--- ProfileFB.py
+++ ProfileFB.py
@@ -12,10 +12,13 @@ def get_userid(token):
 
 def protection(token, enable = True):
 	uid = get_userid(token)
-	data = 'variables={"0":{"is_shielded": %s,"session_id":"9b78191c-84fd-4ab6-b0aa-19b39f04a6bc","actor_id":"%s","client_mutation_id":"b0316dd6-3fd6-4beb-aed4-bb29c5dc64b0"}}&method=post&doc_id=1477043292367183&query_name=IsShieldedSetMutation&strip_defaults=true&strip_nulls=true&locale=en_US&client_country_code=US&fb_api_req_friendly_name=IsShieldedSetMutation&fb_api_caller_class=IsShieldedSetMutation' % (enable, str(uid))
-	headers = {"Content-Type" : "application/x-www-form-urlencoded", "Authorization" : "OAuth %s" % token}
+	d = 'variables={"0":{"is_shielded": %s,
+			"session_id":"9b78191c-84fd-4ab6-b0aa-19b39f04a6bc",
+			"actor_id":"%s",
+			"client_mutation_id":"b0316dd6-3fd6-4beb-aed4-bb29c5dc64b0"}}&method=post&doc_id=1477043292367183&query_name=IsShieldedSetMutation&strip_defaults=true&strip_nulls=true&locale=en_US&client_country_code=US&fb_api_req_friendly_name=IsShieldedSetMutation&fb_api_caller_class=IsShieldedSetMutation' % (enable, str(uid))
+	h = {"Content-Type" : "application/x-www-form-urlencoded", "Authorization" : "OAuth %s" % token}
 	url = "https://graph.facebook.com/graphql"
-	res = requests.post(url, data = data, headers = headers)
+	res = requests.post(url, data = d, headers = h)
 	print("Successfully....")
 
 system("clear")
